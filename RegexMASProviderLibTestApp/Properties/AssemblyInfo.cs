3c8c875eccca44bae529edadbb3717b97c5920df
Emanuel Albu <ealbu@sdl.com>
2025-05-20 09:59:49 +0200

    Regex Match AutoSuggest Provider 3.1.0.X / 2024 SR1

--- RegexMASProviderLibTestApp/Properties/AssemblyInfo.cs
+++ RegexMASProviderLibTestApp/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.4")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
